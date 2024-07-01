544632e1f6c833cd53e6a65bf8e89419cda7c3a0
Raimokarmayuri <mayuriraimokar@gmail.com>
2024-07-01 16:27:36 +0530

    update 2

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -1,93 +1,3 @@
-// import React, { useState, useEffect } from 'react'
-// import { useDispatch, useSelector } from 'react-redux'
-// import { NavLink, useNavigate } from 'react-router-dom'
-// import '../components/header/appheader.css'
-// import {
-//   CContainer,
-//   CHeader,
-//   CHeaderBrand,
-//   CHeaderDivider,
-//   CHeaderNav,
-//   CHeaderToggler,
-//   CNavLink,
-//   CNavItem,
-// } from '@coreui/react'
-// import CIcon from '@coreui/icons-react'
-// import { cilMenu } from '@coreui/icons' // Removed other icons for simplicity
-// import { AppBreadcrumb } from './index'
-// import { useLocation } from 'react-router-dom'
-// import { useLogoutMutation } from 'src/slices/usersApiSlice'
-// import { logout } from '../slices/authSlice'
-
-// const AppHeader = () => {
-//   const sidebarShow = useSelector((state) => state.sidebarShow)
-
-//   const userrole = useSelector((state) => state.auth.userInfo?.role) || ''
-//   const username = useSelector((state) => state.auth.userInfo?.name)
-
-//   const dispatch = useDispatch()
-//   const navigate = useNavigate()
-
-//   const [logoutApiCall] = useLogoutMutation()
-
-//   const logoutHandler = async () => {
-//     try {
-//       await logoutApiCall().unwrap()
-//       dispatch(logout())
-//       navigate('/login')
-//     } catch (error) {}
-//   }
-
-//   return (
-//     <CHeader position="sticky" className="mb-4">
-//       <CContainer fluid>
-//         <CHeaderToggler
-//           className="ps-1"
-//           onClick={() => dispatch({ type: 'set', sidebarShow: !sidebarShow })}
-//         >
-//           <CIcon icon={cilMenu} size="lg" />
-//         </CHeaderToggler>
-//         <CHeaderBrand className="mx-auto d-md-none" to="/">
-//           {/* <CIcon icon={logo} height={48} alt="Logo" /> */}
-//         </CHeaderBrand>
-//         <CHeaderNav className="d-none d-md-flex me-auto">
-//           <CNavItem>
-//             <CNavLink to="/dashboard" component={NavLink}>
-//               Dashboard
-//             </CNavLink>
-//           </CNavItem>
-//           {/* Other navigation items */}
-//         </CHeaderNav>
-//         <CHeaderNav>
-//           <CNavItem>
-//             <h5>Welcome , {username} </h5>
-//             <p>
-//               Role: {userrole} &nbsp;{' '}
-//               <button className="button-91" role="button" onClick={logoutHandler}>
-//                 LogOut
-//               </button>
-//             </p>
-//             {/* <button className="button-91" role="button">
-//               Button 91
-//             </button> */}
-//             {/* <button onClick={logoutHandler}>Logout</button> */}
-//             <CNavLink href="#" style={{ fontSize: '25px', color: 'orange' }}>
-//               {/* {currentUser.name} */}
-//             </CNavLink>
-//           </CNavItem>
-//         </CHeaderNav>
-//       </CContainer>
-//       <CHeaderDivider />
-//       <CContainer fluid>
-//         <AppBreadcrumb />
-//       </CContainer>
-//     </CHeader>
-//   )
-// }
-
-// export default AppHeader
-
-// AppHeader.js
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { NavLink, useNavigate } from 'react-router-dom'
@@ -101,11 +11,6 @@ import {
   CNavLink,
   CNavItem,
   CButton,
-  CModal,
-  CModalHeader,
-  CModalTitle,
-  CModalBody,
-  CModalFooter,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilQrCode, cilBell, cilHelp } from '@coreui/icons'
@@ -117,7 +22,6 @@ import QrScanner from '../views/QrScanner'
 
 const AppHeader = () => {
   const [isQrScannerOpen, setIsQrScannerOpen] = useState(false)
-  const [qrModal, setQrModal] = useState()
   const [scanResult, setScanResult] = useState('')
   const sidebarShow = useSelector((state) => state.custom.sidebarShow)
   const userrole = useSelector((state) => state.auth.userInfo?.plant) || ''
@@ -140,16 +44,9 @@ const AppHeader = () => {
     }
   }
 
-  const handleScan = (data) => {
-    if (data) {
-      console.log('QR Code Data:', data)
-      setScanResult(data.text)
-      setQrModal(false)
-    }
-  }
-
-  const handleError = (error) => {
-    console.error('QR Scanner Error:', error)
+  const handleScanResult = (data) => {
+    setScanResult(data)
+    setIsQrScannerOpen(false) // Close the scanner after successful scan
   }
 
   return (
@@ -176,9 +73,6 @@ const AppHeader = () => {
             <h6>Role: {userrole}</h6>
           </CNavItem>
           <CNavItem>
-            {/* <CButton color="warning" onClick={() => setQrModal(true)}>
-              Scan QR Code
-            </CButton> */}
             <CIcon
               icon={cilQrCode}
               size="3xl"
@@ -192,7 +86,13 @@ const AppHeader = () => {
       <CContainer fluid>
         <AppBreadcrumb />
       </CContainer>
-      {isQrScannerOpen && <QrScanner onClose={toggleQrScanner} />}
+      {isQrScannerOpen && (
+        <QrScanner
+          onScan={handleScanResult}
+          onError={console.error}
+          onClose={() => setIsQrScannerOpen(false)}
+        />
+      )}
     </CHeader>
   )
 }
--- src/views/QrScanner.js
+++ src/views/QrScanner.js
@@ -1,75 +1,32 @@
-import React, { useState, useEffect } from 'react'
-import { QrReader } from 'react-qr-reader'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import './QrScanner.css'
-import { useNavigate } from 'react-router-dom'
+import { QrReader } from '@blackbox-vision/react-qr-reader' // Adjust the import path
 
-const QrScanner = ({ onClose }) => {
-  const [scannedData, setScannedData] = useState(null)
-  const navigate = useNavigate()
-
-  useEffect(() => {
-    const checkCameraAccess = async () => {
-      try {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true })
-        console.log('Camera access granted')
-        alert('Camera access granted')
-        stream.getTracks().forEach((track) => track.stop())
-      } catch (err) {
-        console.error('Camera access denied:', err)
-        alert('Please allow camera access to scan QR codes.')
-      }
-    }
-
-    const checkGetUserMediaSupport = () => {
-      const supportsGetUserMedia = !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia)
-      if (supportsGetUserMedia) {
-        console.log('getUserMedia is supported!')
-        checkCameraAccess()
-      } else {
-        console.error('getUserMedia is not supported on this device/browser.')
-        alert('Your browser does not support getUserMedia. Please use a modern browser.')
-      }
-    }
-
-    checkGetUserMediaSupport()
-  }, [])
+const QRScanner = ({ onScan, onError, onClose }) => {
+  const [delay, setDelay] = useState(500)
 
   const handleScan = (data) => {
     if (data) {
-      setScannedData(data)
-      alert(`Scanned Data: ${data}`)
-      window.open(data, '_blank') // Open the scanned URL in a new tab
-      onClose() // Close the scanner after a successful scan
+      onScan(data)
     }
   }
 
   const handleError = (err) => {
-    console.error('QR Scanner Error:', err)
+    onError(err)
   }
 
   return (
-    <div className="qr-scanner-modal">
-      <div className="qr-scanner-content">
-        <QrReader
-          delay={300}
-          onError={handleError}
-          onResult={(result) => {
-            if (result?.text) {
-              handleScan(result.text)
-            }
-          }}
-          constraints={{ facingMode: 'environment' }}
-          style={{ width: '100%' }}
-        />
-        <button onClick={onClose}>Close</button>
-      </div>
+    <div className="qr-scanner">
+      <QrReader delay={delay} onError={handleError} onScan={handleScan} style={{ width: '100%' }} />
+      <button onClick={onClose}>Close Scanner</button>
     </div>
   )
 }
 
-QrScanner.propTypes = {
+QRScanner.propTypes = {
+  onScan: PropTypes.func.isRequired,
+  onError: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
 }
 
-export default QrScanner
+export default QRScanner
